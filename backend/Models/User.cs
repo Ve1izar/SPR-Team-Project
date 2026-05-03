b36071aa3462371b4d43096bc6b72e9009c020c6
MikeSheyko <zavod.vp.dvp@gmail.com>
2026-05-03 21:00:15 +0300

    Auth, JvtToken

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -3,6 +3,11 @@
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string? FirstName { get; set; } = string.Empty;
+        public string? LastName { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; } = string.Empty;
     }
 }
